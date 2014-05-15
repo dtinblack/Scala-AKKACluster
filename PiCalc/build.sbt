1b5ced7ab0482a91fa05843683b6fae63944216e
David Black <dtinblack@gmail.com>
2014-05-15 16:28:30 +0100

    baseline

---
+++ PiCalc/build.sbt
@@ -0,0 +1,9 @@
+name := "PiCalc"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
+
+
+
---
+++ PiCalc/src/main/scala/PiCalc.scala
@@ -0,0 +1,30 @@
+// Calculating Pi using Scala
+
+package pi_simple
+
+import scala.annotation.tailrec
+
+object Pi {
+   
+  def calculatePi(numberOfElements: Int) : Double = {
+     @tailrec
+     def calculatePiFor(start: Int, limit: Int, acc: Double) : Double =
+       start match {
+         case x if x == limit => acc
+         case _ => calculatePiFor(start + 1, limit, acc + 4.0 * (1 - (start % 2) * 2) / (2 * start + 1))
+       }
+     calculatePiFor(0, numberOfElements , 0.0)
+   }
+   
+  def main(args: Array[String]) {
+  
+    val start = System.currentTimeMillis 
+    
+    val numberOfElements = 1000000
+      
+    val pi = calculatePi(numberOfElements)
+    
+    println(s"\n\tpi approximation: ${pi}, took: ${System.currentTimeMillis - start} millis")
+
+  }
+}
